0e8d105b2202b4f92bd24dc678cbff744a6a1731
GK24 <Genius00024@gmail.com>
2017-10-20 20:18:47 +0100

    fix compile errors

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-rm -rf modules
 export CONFIG_FILE="cyanogenmod_z2_plus_defconfig"
 export ARCH="arm64"
 export CROSS_COMPILE="aarch64-linux-android-"
 export TOOL_CHAIN_PATH="${HOME}/android/current/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/"
 export CONFIG_ABS_PATH="arch/${ARCH}/configs/${CONFIG_FILE}"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
-export objdir="${HOME}/Desktop/z2/obj"
-export sourcedir="${HOME}/Desktop/z2"
+export objdir="${HOME}/Desktop/obj"
+export sourcedir="${HOME}/android/kernel/zuk/z2"
+
 compile() {
   make O=$objdir ARCH=arm64 CROSS_COMPILE=${TOOL_CHAIN_PATH}/${CROSS_COMPILE}  $CONFIG_FILE -j$(nproc --all)
   make O=$objdir -j$(nproc --all)
